21d8369e4178a579469dd1f96bfc85b159bf3e8c
Jose Dinis <jcabicho@gmail.com>
2022-02-16 02:24:17 +0000

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionController } from './transaction/transaction.controller';
-import { TransctionsResolver } from './transctions/transctions.resolver';
+//import { TransctionsResolver } from './transctions/transctions.resolver';
 import { TransactionnsModule } from './transactionns/transactionns.module';
 import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
   imports: [TransactionnsModule, TweetsModule],
   controllers: [AppController, TransactionController],
-  providers: [AppService, TransctionsResolver],
+  providers: [AppService],//, TransctionsResolver],
 })
 export class AppModule {}
